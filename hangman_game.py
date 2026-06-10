156305426588791104dae7f49ccd3325951b8121
Dana Salasin <danasalasin@gmail.com>
2026-06-10 13:12:57 +0300

    Step 1.4.2: Add Hangman ASCII states

--- hangman_game.py
+++ hangman_game.py
@@ -11,3 +11,68 @@ print("""
                      |___/
 """)
 print(random.randint(5, 10))
+
+print("""
+picture 1:
+    x-------x
+""")
+
+print("""
+picture 2:
+    x-------x
+    |
+    |
+    |
+    |
+    |
+""")
+
+print("""
+picture 3:
+    x-------x
+    |       |
+    |       0
+    |
+    |
+    |
+""")
+
+print("""
+picture 4:
+    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |
+""")
+
+print("""
+picture 5:
+    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |
+""")
+
+print("""
+picture 6:
+    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      /
+    |
+""")
+
+print("""
+picture 7:
+    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      / \\
+    |
+""")
