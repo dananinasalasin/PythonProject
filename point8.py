251105c6ceed5501a070fe57963891e4338fcbbd
Dana Salasin <danasalasin@gmail.com>
2026-06-13 15:37:40 +0300

    Step 8.2.2

--- point8.py
+++ point8.py
@@ -3,4 +3,9 @@ data = ("self", "py", 1.543)
 format_string = "Hello %s.%s learner, you have only %.1f units left before you master the course!"
 print(format_string % data)
 
+# 8.2.2
+def sort_prices(list_of_tuples):
+    return sorted(list_of_tuples, key=lambda item: float(item[1]), reverse=True)
 
+products = [('milk', '5.5'), ('candy', '2.5'), ('bread', '9.0')]
+print(sort_prices(products))
