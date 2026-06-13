c27b865afe8cbc31b6111e6e327af477b8aaf14b
Dana Salasin <danasalasin@gmail.com>
2026-06-13 14:26:23 +0300

    Step 7.2.1

--- point7.py
+++ point7.py
@@ -6,5 +6,15 @@ def squared_numbers(start, stop):
         start += 1
     return list_squares
 
-print(squared_numbers(4, 8))
+#print(squared_numbers(4, 8))
 
+# 7.2.1
+def is_greater(my_list, n):
+    list_greater = []
+    for num in my_list:
+        if num > n:
+            list_greater.append(num)
+    return list_greater
+
+result = is_greater([1, 30, 25, 60, 27, 28], 28)
+print(result)
